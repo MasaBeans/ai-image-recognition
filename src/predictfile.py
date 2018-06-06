4542eb843fbdf08af84995679b615136e9cae0d1
nosem <0323.nose@gmail.com>
2018-06-06 23:31:37 +0900

    ファイルアップロードのプログラム作成

--- src/predictfile.py
+++ src/predictfile.py
@@ -1,13 +1,48 @@
 import os
-from flask import Flask, request, redirect, url_for  # redirectはページ遷移に使う
-from werkzeug.utils import secure_filename # アップロードされたファイル名に危険なコマンドが含まれている場合それを除去する
-
+from flask import Flask, request, redirect, url_for
+from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = './uploads'
-ALLOWD_EXTENSIONS = set(['png', 'jpg', 'gif'])
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'gif'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-def allowd_file(filename):
-    return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWD_EXTENSIONS
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('file is not in')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('file is not in')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+        return '''
+        <!doctype html>
+        <html>
+        <head>
+        <meta charset="UTF-8">
+        <title>FILE UPLOAD</title></head>
+        <body>
+        <h1>YOU CAN UPLOAD A FILE!</h1>
+        <form method = post enctype = multipart/form-data>
+        <input type=file name=file>
+        <input type=submit value=Upload>
+        </form>
+        </body>
+        </html>
+        '''
+
+from flask import send_from_directory
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
